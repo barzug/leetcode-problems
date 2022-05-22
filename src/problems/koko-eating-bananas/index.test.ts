f218a287aaac07d0d906b3807c626f49190fc221
Sergey Barsukov <s.barsukov@corp.mail.ru>
2022-05-22 14:39:54 +0300

    koko-eating-bananas

---
+++ src/problems/koko-eating-bananas/index.test.ts
@@ -0,0 +1,18 @@
+import path from 'path';
+
+import problemFunction from './index';
+
+describe(path.basename(__dirname), () => {
+	it('should work with test case 1', () => {
+		expect(problemFunction([3,6,7,11], 8)).toBe(4);
+	});
+
+	it('should work with test case 2', () => {
+		expect(problemFunction([30,11,23,4,20], 5)).toBe(30);
+
+	});
+
+	it('should work with test case 3', () => {
+		expect(problemFunction([30,11,23,4,20], 6)).toBe(23);
+	});
+});
---
+++ src/problems/koko-eating-bananas/index.ts
@@ -0,0 +1,27 @@
+function isValid(piles: number[], h: number, k: number): boolean {
+	let count = 0;
+
+	piles.forEach((value) => {
+		count += Math.ceil(value / k);
+	});
+
+	return count <= h;
+}
+
+export default function minEatingSpeed(piles: number[], h: number): number {
+	let l = 1;
+	let r = Math.max(...piles);
+
+	while (l < r) {
+		const mid = l + Math.floor((r - l) / 2);
+
+		if (isValid(piles, h, mid)) {
+			r = mid;
+		} else {
+			l = mid + 1;
+		}
+
+	}
+
+	return l;
+};
