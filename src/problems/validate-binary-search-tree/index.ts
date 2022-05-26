ac1d325e1ff266bd0f128345f0ff82bcb2a7f443
Sergey Barsukov <s.barsukov@corp.mail.ru>
2022-05-26 09:29:32 +0300

    validate-binary-search-tree

---
+++ src/problems/validate-binary-search-tree/index.ts
@@ -0,0 +1,33 @@
+import {TreeNode} from '@/algorithms/createBinaryTreeFromArray';
+
+function dfs(root: TreeNode, min: number, max: number): boolean {
+	if (root.val <= min || root.val >= max) {
+		return false;
+	}
+
+
+	if (root.left !== null && typeof root.left !== 'undefined') {
+		const _max = Math.min(max, root.val);
+		if (!dfs(root.left, min, _max)) {
+			return false;
+		}
+	}
+
+	if (root.right !== null && typeof root.right !== 'undefined') {
+		const _min = Math.max(min, root.val);
+		if (!dfs(root.right, _min, max)) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+
+export default function isValidBST(root: TreeNode | null): boolean {
+	if (!root) {
+		return true;
+	}
+
+	return dfs(root, -Infinity, Infinity);
+};
