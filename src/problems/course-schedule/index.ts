9de7ba36e658e2e4101a77c18a66eaaa8cef3702
Sergey Barsukov <s.barsukov@corp.mail.ru>
2022-05-29 10:03:54 +0300

    course-schedule

---
+++ src/problems/course-schedule/index.ts
@@ -0,0 +1,38 @@
+export default function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+	const graph: Array<Array<number>> = new Array(numCourses).fill(null).map(() => []);
+	const nodesState = new Array(numCourses).fill(0);
+
+	prerequisites.forEach((value) => {
+		graph[value[1]].push(value[0]);
+	});
+
+	function dfs(index: number): boolean {
+		// visited in current dfs search
+		if (nodesState[index] === 1) {
+			return false;
+		}
+
+		// already checked
+		if (nodesState[index] === 2) {
+			return true;
+		}
+
+
+		nodesState[index] = 1;
+
+		const result = graph[index].every((value) => dfs(value));
+
+		nodesState[index] = 2;
+
+		return result;
+	}
+
+
+	return graph.every((_, index) => {
+		if (nodesState[index] === 0) {
+			return dfs(index);
+		}
+
+		return true;
+	});
+};
