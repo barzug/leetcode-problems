d8ac4e4672633e54f8b329dfaf82fe68f1727fc4
Sergey Barsukov <s.barsukov@corp.mail.ru>
2022-05-29 10:47:19 +0300

    course-schedule-2

---
+++ src/problems/course-schedule-2/index.test.ts
@@ -0,0 +1,18 @@
+import path from 'path';
+
+import problemFunction from './index';
+
+describe(path.basename(__dirname), () => {
+	it('should work with test case 1', () => {
+		expect(problemFunction(2, [[1, 0]])).toEqual([0, 1]);
+	});
+
+	it('should work with test case 2', () => {
+		expect(problemFunction(4, [[1, 0], [2, 0], [3, 1], [3, 2]]),).toEqual([0, 2, 1, 3]);
+	});
+
+	it('should work with test case 3', () => {
+		expect(problemFunction(1, [])).toEqual([0]);
+	});
+
+});
---
+++ src/problems/course-schedule-2/index.ts
@@ -0,0 +1,40 @@
+export default function findOrder(numCourses: number, prerequisites: number[][]): number[] {
+	const graph: Array<Array<number>> = new Array(numCourses).fill(null).map(() => []);
+	const nodesState = new Array(numCourses).fill(0);
+	const result: number[] = [];
+
+	prerequisites.forEach((value) => {
+		graph[value[1]].push(value[0]);
+	});
+
+	function dfs(index: number): boolean {
+		// visited in current dfs search
+		if (nodesState[index] === 1) {
+			return false;
+		}
+
+		// already checked
+		if (nodesState[index] === 2) {
+			return true;
+		}
+
+		nodesState[index] = 1;
+
+		const withoutCycles = graph[index].every((value) => dfs(value));
+
+		result.push(index);
+		nodesState[index] = 2;
+
+		return withoutCycles;
+	}
+
+	const withoutCycles = graph.every((_, index) => {
+		if (nodesState[index] === 0) {
+			return dfs(index);
+		}
+
+		return true;
+	});
+
+	return  withoutCycles ? result.reverse() : [];
+}
