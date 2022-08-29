d4e8302b6c98977ed53ad10ab90b1b719b1d440c
Sergey Barsukov <s.barsukov@corp.mail.ru>
2022-08-29 10:35:23 +0300

    top-k-frequent-elements

---
+++ src/problems/top-k-frequent-elements/index.ts
@@ -0,0 +1,32 @@
+export default function topKFrequent(nums: number[], k: number): number[] {
+	// bruteforce - create map, sort by frequent = O(nlogn)
+	// more optimal - array by frequency
+
+	const freqMap = new Map();
+	const freqArrayOfArrays: number[][] = [];
+
+	nums.forEach((value) => freqMap.set(value, (freqMap.get(value) || 0) + 1));
+
+	freqMap.forEach((value, key) => {
+		if (!freqArrayOfArrays[value]) {
+			freqArrayOfArrays[value] = [];
+		}
+
+		freqArrayOfArrays[value].push(key);
+	});
+
+	const result = [];
+	for (let i = freqArrayOfArrays.length - 1; i >= 0; i--) {
+		if (!freqArrayOfArrays[i]) {
+			continue;
+		}
+
+		result.push(...freqArrayOfArrays[i])
+
+		if (result.length >= k) {
+			return result
+		}
+	}
+
+	return result;
+};
