2aa81b7d805f86cd6a38156563d31912d0878912
Sergey Barsukov <s.barsukov@corp.mail.ru>
2022-09-03 09:24:16 +0300

    two-sum

---
+++ src/problems/two-sum/index.ts
@@ -0,0 +1,11 @@
+export default function twoSum(nums: number[], target: number): number[] {
+	const map = {};
+
+	for (let i = 0; i < nums.length; i++) {
+		if (target - nums[i] in map) {
+			return [map[target - nums[i]], i]
+		}
+
+		map[nums[i]] = i;
+	}
+};
