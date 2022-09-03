8f06a78efe07ce713417bc58818a201f1b19cc6b
Sergey Barsukov <s.barsukov@corp.mail.ru>
2022-09-03 14:16:18 +0300

    integer-to-roman

---
+++ src/problems/integer-to-roman/index.test.ts
@@ -0,0 +1,17 @@
+import path from 'path';
+
+import problemFunction from './index';
+
+describe(path.basename(__dirname), () => {
+	it('should work with test case 1', () => {
+		expect(problemFunction(3)).toBe("III");
+	});
+
+	it('should work with test case 2', () => {
+		expect(problemFunction(58)).toBe("LVIII");
+	});
+
+	it('should work with test case 3', () => {
+		expect(problemFunction(1994)).toBe("MCMXCIV");
+	});
+});
