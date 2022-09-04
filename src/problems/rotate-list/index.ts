39422749f2247c9c8d9b558e71f6a751d8964d00
Sergey Barsukov <s.barsukov@corp.mail.ru>
2022-09-04 09:26:36 +0300

    rotate-list

---
+++ src/problems/rotate-list/index.ts
@@ -0,0 +1,39 @@
+class ListNode {
+	val: number;
+	next: ListNode | null;
+
+	constructor(val?: number, next?: ListNode | null) {
+		this.val = val === undefined ? 0 : val;
+		this.next = next === undefined ? null : next;
+	}
+}
+
+function rotateRight(head: ListNode | null, k: number): ListNode | null {
+	if (!head) {
+		return null;
+	}
+
+	let listLength = 1;
+
+	let pointer = head;
+	while (pointer.next !== null) {
+		pointer = pointer.next;
+		listLength++;
+	}
+
+	let numberOfSteps = k % listLength;
+	if (numberOfSteps === 0) {
+		return head;
+	}
+
+	pointer.next = head;
+	while (listLength > numberOfSteps) {
+		pointer = pointer.next;
+		numberOfSteps++;
+	}
+
+	const newHead = pointer.next;
+	pointer.next = null;
+
+	return newHead
+}
