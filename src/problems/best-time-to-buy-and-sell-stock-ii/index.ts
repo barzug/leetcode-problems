2486f0aafea955d9261a4dc32ed8b40fbad363a8
Sergey Barsukov <s.barsukov@corp.mail.ru>
2022-09-14 10:48:13 +0300

    best-time-to-buy-and-sell-stock-ii optimization

--- src/problems/best-time-to-buy-and-sell-stock-ii/index.ts
+++ src/problems/best-time-to-buy-and-sell-stock-ii/index.ts
@@ -1,18 +1,11 @@
 export default function maxProfit(prices: number[]): number {
 	let summ = 0;
 
-	let current = 0;
 	for (let i = 0; i < prices.length - 1; i++) {
-		if (prices[i + 1] < prices[i]) {
-			summ += current;
-			current = 0;
-			continue;
+		if (prices[i + 1] > prices[i]) {
+			summ +=  prices[i + 1] - prices[i];
 		}
-
-		current +=  prices[i + 1] - prices[i];
 	}
-
-	summ += current;
 
 	return summ
 };
