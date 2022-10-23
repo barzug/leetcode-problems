c12e13561a43de540cc3bd0630a25dc2716e0d08
Sergey Barsukov <s.barsukov@corp.mail.ru>
2022-10-23 16:48:28 +0300

    implement-queue-using-stacks

---
+++ src/problems/implement-queue-using-stacks/index.ts
@@ -0,0 +1,38 @@
+class MyQueue {
+	inputStack: number[] = [];
+	outputStack: number[] = [];
+
+	_fillOutputStack() {
+		while (this.inputStack.length !== 0) {
+			this.outputStack.push(this.inputStack.pop())
+		}
+	}
+
+	push(x: number): void {
+		this.inputStack.push(x);
+	}
+
+	pop(): number {
+		if (this.outputStack.length) {
+			return this.outputStack.pop();
+		}
+
+		this._fillOutputStack()
+
+		return this.outputStack.pop();
+	}
+
+	peek(): number {
+		if (this.outputStack.length) {
+			return this.outputStack[this.outputStack.length - 1];
+		}
+
+		this._fillOutputStack()
+
+		return this.outputStack[this.outputStack.length - 1];
+	}
+
+	empty(): boolean {
+		return (this.outputStack.length === 0 && this.inputStack.length === 0);
+	}
+}
