998a19318ec12c0657682d752e909c5810efcabc
Sergey Barsukov <s.barsukov@corp.mail.ru>
2022-10-25 10:09:10 +0300

    climbing-stairs

---
+++ src/problems/climbing-stairs/index.ts
@@ -0,0 +1,15 @@
+export default function climbStairs(n: number): number {
+	if (n < 3) {
+		return n;
+	}
+
+	let prev = 1;
+	let current = 2;
+	for (let i = 3; i <= n; i++) {
+		let newCurrent = prev + current
+		prev = current;
+		current = newCurrent;
+	}
+
+	return current;
+};
